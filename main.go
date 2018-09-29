8e597f7e1b913686d2b98d817853c6bc30e5ef33
Dnnd <ddann9@live.com>
2018-09-29 18:23:49 +0300

    add router config

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"github.com/TeamD2018/geo-rest/controllers"
+	"github.com/TeamD2018/geo-rest/services"
+	"github.com/gin-gonic/gin"
+	"github.com/olivere/elastic"
+	"go.uber.org/zap"
 	"log"
 )
 
 func main() {
-	log.Printf("Server started")
+	elasticClient, err := elastic.NewClient(elastic.SetURL("http://elastic:9200"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal(err)
+	}
+	api := controllers.APIService{
+		OrdersDAO: services.NewOrdersElasticDAO(elasticClient, logger, ""),
+		Logger:    logger,
+	}
+	router := gin.Default()
+	router.POST("/couriers/:courier_id/orders", api.CreateOrder)
+	router.GET("/couriers/:courier_id/orders/:order_id", api.GetOrder)
+	router.PUT("/couriers/:courier_id/orders/:order_id", api.UpdateOrder)
+	router.PATCH("/couriers/:courier_id/orders/:order_id", api.AssignNewCourier)
+	router.DELETE("/couriers/:courier_id/orders/:order_id", api.DeleteOrder)
+	router.Run(":8080")
 }
