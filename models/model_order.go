ca8202671c8a9cc45c9f77f9e751a74bc25d1100
Dnnd <ddann9@live.com>
2018-09-29 14:47:04 +0300

    fix order model

--- models/model_order.go
+++ models/model_order.go
@@ -14,7 +14,33 @@ type Order struct {
 	// Order cancellation time in UTC format(ms)
 	DeliveredAt int64 `json:"delivered_at"`
 
+	Destination Location `json:"destination,omitempty"`
+
+	Source Location `json:"source,omitempty"`
+}
+
+type OrderCreate struct {
+	CourierID   *string   `json:"courier_id"`
+	Destination Location `json:"destination"`
+	Source      Location `json:"source"`
+}
+
+type OrderUpdate struct {
+	// Order id
+	ID *string `json:"id,omitempty"`
+
+	// Courier id
+	CourierID *string `json:"courier_id,omitempty"`
+
+	// Order creation time in UTC format(ms)
+	Created *int64 `json:"created,omitempty"`
+
+	// Order cancellation time in UTC format(ms)
+	EndTime *int64 `json:"end_time,omitempty"`
+
+	//Destination of order
 	Destination *Location `json:"destination,omitempty"`
 
+	//Source of order
 	Source *Location `json:"source,omitempty"`
 }
