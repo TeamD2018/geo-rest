f4cc92807be2ce0486ee6b47dddb2071dcc11c78
oleggator <oleg@utkin.xyz>
2018-10-03 01:02:24 +0300

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ echo "Deploying to $SERVER_IP"
 
 echo "Setting up ssh..."
 eval "$(ssh-agent -s)"
-ssh-keyscan -H $SWARM_MANAGER_IP >> ~/.ssh/known_hosts
+ssh-keyscan -p 228 -H $SWARM_MANAGER_IP >> ~/.ssh/known_hosts
 chmod 600 travis_key
 ssh-add travis_key
 
 echo "Uploading..."
-scp -P228 -r docker-compose.yml $DEPLOY_USER@$SWARM_MANAGER_IP:/tmp/
+scp -P 228 -r docker-compose.yml $DEPLOY_USER@$SWARM_MANAGER_IP:/tmp/
 
 echo "Pushing stack to swarm..."
-ssh -p228 $DEPLOY_USER@$SWARM_MANAGER_IP "docker stack deploy --compose-file /tmp/docker-compose.yml geo-rest"
+ssh -p 228 $DEPLOY_USER@$SWARM_MANAGER_IP "docker stack deploy --compose-file /tmp/docker-compose.yml geo-rest"
