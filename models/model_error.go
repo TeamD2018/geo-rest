e75b013d3e3f48c75e2610dfb5fbaea7f0a287fb
zwirec <zwirec@icloud.com>
2018-10-02 18:39:22 +0300

    merge

--- models/model_error.go
+++ models/model_error.go
@@ -1,19 +1,29 @@
 package models
 
+import "fmt"
+
 type Error struct {
-	Message string
-	Code    int
+	Parameter interface{}
+	Message   string
+	Code      int
 }
 
 func (e Error) Error() string {
-	return e.Message
+	return fmt.Sprintf("code: %d message: %s", e.Code, fmt.Sprintf(e.Message, e.Parameter))
+}
+
+func (e *Error) SetParameter(p interface{}) *Error {
+	e.Parameter = p
+	return e
 }
 
 // List of Error
 var (
-	CourierAlreadyExists              = Error{"Courier already exists", 10}
-	ServerError                       = Error{"Sorry, server error", 20}
-	OneOfParametersNotFound           = Error{"One of parameters not found", 30}
-	OneOfParameterHaveIncorrectFormat = Error{"One of parameter have incorrect format", 40}
-	EntityNotFound                    = Error{"Entity with such id not found", 50}
+	ErrCourierAlreadyExists              = Error{Message: "Courier with id %s already exists", Code: 10}
+	ErrServerError                       = Error{Message: "Sorry, server error", Code: 20}
+	ErrCourierNotFound                   = Error{Message: "Courier with id %s not found", Code: 60}
+	ErrOneOfParametersNotFound           = Error{Message: "One of parameters not found", Code: 30}
+	ErrOneOfParameterHaveIncorrectFormat = Error{Message: "One of parameter have incorrect format", Code: 40}
+	ErrEntityNotFound                    = Error{Message: "Entity with such id not found", Code: 50}
+	ErrUnmarshalJSON                     = Error{Message: "Error with unmarshal JSON: %s"}
 )
