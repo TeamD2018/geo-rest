b7cc3113a27b092795bf2102df6db738db30238a
Dnnd <ddann9@live.com>
2018-10-04 23:47:25 +0300

    initialize gmaps resolver service

--- main.go
+++ main.go
@@ -7,11 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/olivere/elastic"
 	"go.uber.org/zap"
+	"googlemaps.github.io/maps"
 	"log"
 )
 
 func main() {
 	elasticClient, err := elastic.NewClient(elastic.SetURL("http://elastic:9200"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	gmaps, err := maps.NewClient(maps.WithAPIKey("AIzaSyCsvYa45nNh7NNLE_PUix8SOI73_HlcTX8"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,6 +26,7 @@ func main() {
 	}
 	couriersDao := services.NewCouriersElasticDAO(elasticClient, logger, "")
 	ordersDao := services.NewOrdersElasticDAO(elasticClient, logger, couriersDao, "")
+	gmapsResolver := services.NewGMapsResolver(gmaps, logger)
 	if err := couriersDao.EnsureMapping();
 		err != nil {
 		logger.Fatal("Fail to ensure couriers mapping: ", zap.Error(err))
@@ -33,6 +39,7 @@ func main() {
 	api := controllers.APIService{
 		CouriersDAO: couriersDao,
 		OrdersDAO:   ordersDao,
+		GeoResolver: gmapsResolver,
 		Logger:      logger,
 	}
 	router := gin.Default()
