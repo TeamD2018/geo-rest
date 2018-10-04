e4d6e2f71b6e643d3beb1ccf1aea3e839513d312
Dnnd <ddann9@live.com>
2018-10-04 15:41:59 +0300

    introduce separate function to configure routes

---
+++ controllers/routers.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouters(router *gin.Engine, api *APIService) {
+	g := router.Group(`/couriers`)
+	//orders endpoints
+	g.POST("/:courier_id/orders", api.CreateOrder)
+	g.GET("/:courier_id/orders/:order_id", api.GetOrder)
+	g.PUT("/:courier_id/orders/:order_id", api.UpdateOrder)
+	g.PATCH("/:courier_id/orders/:order_id", api.AssignNewCourier)
+	g.DELETE("/:courier_id/orders/:order_id", api.DeleteOrder)
+	g.GET("/:courier_id/orders", api.GetOrdersForCourier)
+
+	//couriers endpoints
+	g.POST("", api.CreateCourier)
+	g.GET("/:courier_id", api.GetCourierByID)
+	g.PUT("/:courier_id", api.UpdateCourier)
+	g.DELETE("/:courier_id", api.DeleteCourier)
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/TeamD2018/geo-rest/controllers"
+	"github.com/TeamD2018/geo-rest/routers"
 	"github.com/TeamD2018/geo-rest/services"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -33,20 +34,7 @@ func main() {
 	config.AllowOrigins = []string{"http://dc.utkin.xyz:8080", "http://35.204.198.186:8080"}
 	router.Use(cors.New(config))
 
-	g := router.Group(`/couriers`)
-	//orders endpoints
-	g.POST("/:courier_id/orders", api.CreateOrder)
-	g.GET("/:courier_id/orders/:order_id", api.GetOrder)
-	g.PUT("/:courier_id/orders/:order_id", api.UpdateOrder)
-	g.PATCH("/:courier_id/orders/:order_id", api.AssignNewCourier)
-	g.DELETE("/:courier_id/orders/:order_id", api.DeleteOrder)
-	g.GET("/:courier_id/orders", api.GetOrdersForCourier)
-
-	//couriers endpoints
-	g.POST("", api.CreateCourier)
-	g.GET("/:courier_id", api.GetCourierByID)
-	g.PUT("/:courier_id", api.UpdateCourier)
-	g.DELETE("/:courier_id", api.DeleteCourier)
+	routers.SetupRouters(router, &api)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
