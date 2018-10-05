3ae1adf0962771224c916b58c196e16697420a4d
oleggator <oleg@utkin.xyz>
2018-10-05 22:30:30 +0300

    add tests for courier controllers

---
+++ controllers/mocks/couriers_dao_mock.go
@@ -0,0 +1,45 @@
+package mocks
+
+import (
+	"github.com/TeamD2018/geo-rest/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type CouriersDAOMock struct {
+	mock.Mock
+}
+
+func (c *CouriersDAOMock) GetByID(courierID string) (*models.Courier, error) {
+	args := c.Called(courierID)
+	return args.Get(0).(*models.Courier), args.Error(1)
+}
+
+func (c *CouriersDAOMock) GetByName(name string) (models.Couriers, error) {
+	args := c.Called(name)
+	return args.Get(0).(models.Couriers), args.Error(1)
+}
+
+func (c *CouriersDAOMock) GetByBoxField(field *models.BoxField) (models.Couriers, error) {
+	args := c.Called(field)
+	return args.Get(0).(models.Couriers), args.Error(1)
+}
+
+func (c *CouriersDAOMock) GetByCircleField(field *models.CircleField) (models.Couriers, error) {
+	args := c.Called(field)
+	return args.Get(0).(models.Couriers), args.Error(1)
+}
+
+func (c *CouriersDAOMock) Create(courier *models.CourierCreate) (*models.Courier, error) {
+	args := c.Called(courier)
+	return args.Get(0).(*models.Courier), args.Error(1)
+}
+
+func (c *CouriersDAOMock) Update(courier *models.CourierUpdate) (*models.Courier, error) {
+	args := c.Called(courier)
+	return args.Get(0).(*models.Courier), args.Error(1)
+}
+
+func (c *CouriersDAOMock) Delete(courierID string) error {
+	args := c.Called(courierID)
+	return args.Error(0)
+}
