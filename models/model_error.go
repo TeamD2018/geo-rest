0486586ad93e1e0447a13cea45961abc58c9585d
Dnnd <ddann9@live.com>
2018-10-06 22:02:36 +0300

    include http code in errors

--- models/model_error.go
+++ models/model_error.go
@@ -1,15 +1,23 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Error struct {
 	Parameter interface{} `json:"-"`
+	HttpCode  int         `json:"-"`
 	Message   string      `json:"msg"`
 	Code      int         `json:"code"`
 }
 
 func (e Error) Error() string {
 	return fmt.Sprintf("code: %d message: %s", e.Code, fmt.Sprintf(e.Message, e.Parameter))
+}
+
+func (e *Error) HttpStatus() int {
+	return e.HttpCode
 }
 
 func (e *Error) SetParameter(p interface{}) *Error {
@@ -19,11 +27,11 @@ func (e *Error) SetParameter(p interface{}) *Error {
 
 // List of Error
 var (
-	ErrCourierAlreadyExists              = Error{Message: "Courier with id %s already exists", Code: 10}
-	ErrServerError                       = Error{Message: "Sorry, server error", Code: 20}
-	ErrCourierNotFound                   = Error{Message: "Courier with id %s not found", Code: 60}
-	ErrOneOfParametersNotFound           = Error{Message: "One of parameters not found", Code: 30}
-	ErrOneOfParameterHaveIncorrectFormat = Error{Message: "One of parameter (%s) have incorrect format", Code: 40}
-	ErrEntityNotFound                    = Error{Message: "Entity with such id %s not found", Code: 50}
-	ErrUnmarshalJSON                     = Error{Message: "Error with unmarshal JSON: %s"}
+	ErrCourierAlreadyExists              = Error{Message: "Courier with id %s already exists", Code: 10, HttpCode: http.StatusConflict}
+	ErrServerError                       = Error{Message: "Sorry, server error", Code: 20, HttpCode: http.StatusInternalServerError}
+	ErrCourierNotFound                   = Error{Message: "Courier with id %s not found", Code: 60, HttpCode: http.StatusNotFound}
+	ErrOneOfParametersNotFound           = Error{Message: "One of parameters not found", Code: 30, HttpCode: http.StatusBadRequest}
+	ErrOneOfParameterHaveIncorrectFormat = Error{Message: "One of parameter (%s) have incorrect format", Code: 40, HttpCode: http.StatusBadRequest}
+	ErrEntityNotFound                    = Error{Message: "Entity with such id %s not found", Code: 50, HttpCode: http.StatusNotFound}
+	ErrUnmarshalJSON                     = Error{Message: "Error with unmarshal JSON: %s", Code: 70, HttpCode: http.StatusBadRequest}
 )
