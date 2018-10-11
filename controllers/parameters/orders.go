08b0c4de202d5ed2a8ec53a7ed0b2e9665a3f915
Dnnd <ddann9@live.com>
2018-10-11 23:37:52 +0300

    add type-safe created and  delivered flags to GetOrdersForCourier method

--- controllers/parameters/orders.go
+++ controllers/parameters/orders.go
@@ -1,6 +1,16 @@
 package parameters
 
+type DirectionFlag bool
+type DeliveredFlag bool
+
+const WithLowerTreshold DirectionFlag = true
+const WithUpperTreshold DirectionFlag = false
+
+const IncludeDelivered DeliveredFlag = false
+const ExcludeDelivered DeliveredFlag = true
+
 type GetOrdersForCourierParams struct {
-	Since int64 `form:"since"`
-	Asc   bool  `form:"asc"`
+	Since            int64         `form:"since"`
+	Asc              DirectionFlag `form:"asc"`
+	ExcludeDelivered DeliveredFlag `form:"exclude_delivered"`
 }
--- services/interfaces/orders_dao.go
+++ services/interfaces/orders_dao.go
@@ -1,11 +1,17 @@
 package interfaces
 
-import "github.com/TeamD2018/geo-rest/models"
+import (
+	"github.com/TeamD2018/geo-rest/controllers/parameters"
+	"github.com/TeamD2018/geo-rest/models"
+)
 
 type IOrdersDao interface {
 	Get(orderID string) (*models.Order, error)
 	Create(order *models.OrderCreate) (*models.Order, error)
 	Update(order *models.OrderUpdate) (*models.Order, error)
 	Delete(orderID string) error
-	GetOrdersForCourier(courierID string, since int64, asc bool) (models.Orders, error)
+	GetOrdersForCourier(courierID string,
+		since int64,
+		asc parameters.DirectionFlag,
+		excludeDelivered parameters.DeliveredFlag) (models.Orders, error)
 }
