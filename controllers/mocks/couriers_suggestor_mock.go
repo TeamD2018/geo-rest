b598befa24de8319aa92d1f6a4d6bca0decee2ed
Dnnd <ddann9@live.com>
2018-10-18 23:57:21 +0300

    fix suggester mock

--- controllers/mocks/couriers_suggestor_mock.go
+++ controllers/mocks/couriers_suggestor_mock.go
@@ -3,6 +3,7 @@ package mocks
 import (
 	"github.com/TeamD2018/geo-rest/controllers/parameters"
 	"github.com/TeamD2018/geo-rest/models"
+	"github.com/TeamD2018/geo-rest/services/interfaces"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -10,11 +11,17 @@ type CouriersSuggestorMock struct {
 	mock.Mock
 }
 
+func (os *CouriersSuggestorMock) SetFuzziness(fuzziness int) interfaces.CourierSuggester {
+	os.Called(fuzziness)
+	return os
+}
+
+func (os *CouriersSuggestorMock) SetFuzzinessThreshold(threshold int) interfaces.CourierSuggester {
+	os.Called(threshold)
+	return os
+}
+
 func (os *CouriersSuggestorMock) Suggest(field string, suggestion *parameters.Suggestion) (models.Couriers, error) {
 	args := os.Called(field, suggestion)
 	return args.Get(0).(models.Couriers), args.Error(1)
-}
-
-func (os *CouriersSuggestorMock) SetFuzziness(fuzziness int) {
-	os.Called(fuzziness)
 }
