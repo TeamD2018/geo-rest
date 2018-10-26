cf644f0e3200d5495f3e46b7f2198aed3443331a
Dnnd <ddann9@live.com>
2018-10-26 21:44:17 +0300

    add orders suggest engine

---
+++ services/orders_suggest_engine.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"github.com/TeamD2018/geo-rest/services/interfaces"
+	"github.com/olivere/elastic"
+)
+
+type OrdersSuggestEngine struct {
+	Field              string
+	Fuzziness          string
+	Index              string
+	Limit              int
+	FuzzinessThreshold int
+}
+
+func (ose *OrdersSuggestEngine) ParseSearchResponse(result *elastic.SearchResult) interfaces.EngineSuggestResults {
+	if result.TotalHits() == 0 {
+		return nil
+	}
+	results := make(interfaces.EngineSuggestResults, 0, result.TotalHits())
+	for _, hit := range result.Hits.Hits {
+		results = append(results, interfaces.SuggestResult{Id: hit.Id, Source: hit.Source})
+	}
+	return results
+}
+
+func (ose *OrdersSuggestEngine) CreateSearchRequest(input string) (*elastic.SearchRequest) {
+	query := elastic.NewMatchQuery(ose.Field, input).Operator("and")
+	if len(input) >= ose.FuzzinessThreshold {
+		query.Fuzziness(ose.Fuzziness)
+	}
+	source := elastic.NewSearchSource().Query(query).Size(ose.Limit)
+	return elastic.NewSearchRequest().SearchSource(source).Index(ose.Index).Type("_doc")
+}
