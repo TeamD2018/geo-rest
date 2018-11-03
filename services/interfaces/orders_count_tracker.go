9fb2b50f75f37625abe4e58ebc5b55c1103ba8b8
Daniil Kotelnikov <zwirec@icloud.com>
2018-11-03 19:38:20 +0300

    typo fix

--- services/interfaces/orders_count_tracker.go
+++ services/interfaces/orders_count_tracker.go
@@ -3,7 +3,7 @@ package interfaces
 import "github.com/TeamD2018/geo-rest/models"
 
 type OrdersCountTracker interface {
-	Inc(courirID string) error
+	Inc(courierID string) error
 	Dec(courierID string) error
 	Sync(ids models.Couriers) (error)
 	Drop(courierID string) error
