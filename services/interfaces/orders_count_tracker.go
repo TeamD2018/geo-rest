d30e9eb7fe83fedc33b66c6abb14410d9a53b75a
Dnnd <ddann9@live.com>
2018-11-05 16:06:13 +0300

    add DecAndGet, IncAndGet  methods to OrdersCountTracker interface

--- services/interfaces/orders_count_tracker.go
+++ services/interfaces/orders_count_tracker.go
@@ -3,8 +3,10 @@ package interfaces
 import "github.com/TeamD2018/geo-rest/models"
 
 type OrdersCountTracker interface {
-	Inc(courierID string) error
-	Dec(courierID string) error
+	Inc(courierID string) (error)
+	Dec(courierID string) (error)
+	DecAndGet(courierID string) (int, error)
+	IncAndGet(courierID string) (int, error)
 	Sync(ids models.Couriers) (error)
 	Drop(courierID string) error
 }
